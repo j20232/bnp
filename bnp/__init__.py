dbf6dd31dce954bb0a4af816a366ed90870ba494
mocobt <mocobt@gmail.com>
2020-07-07 03:33:09 +0900

    fix typo

--- bnp/__init__.py
+++ bnp/__init__.py
@@ -1,10 +1,10 @@
 from bnp import io
-from bnp import convertion
+from bnp import conversion
 __version__ = '0.1.0'
 
 import importlib
 importlib.reload(io)
-importlib.reload(convertion)
+importlib.reload(conversion)
 
 if True:
     from bnp.conversion import any2np
