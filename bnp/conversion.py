ab916836418c1e389d5891150875c77e63e0da25
mocobt <mocobt@gmail.com>
2020-07-07 03:48:38 +0900

    refactoring

--- bnp/conversion.py
+++ bnp/conversion.py
@@ -4,6 +4,8 @@ import numpy as np
 
 
 def any2np(obj, dtype=np.float32, is_local=False):
+    if type(obj) == mathutils.Vector:
+        return vec2np(obj, dtype=dtype)
     if type(obj) == mathutils.Matrix:
         return mat2np(obj, dtype=dtype)
     elif type(obj) == bpy.types.ObjectType:
@@ -14,31 +16,27 @@ def any2np(obj, dtype=np.float32, is_local=False):
         raise NotImplementedError(f"{type(obj)} is not supported.")
 
 
+def vec2np(vec, dtype=np.float32):
+    return np.array([v for v in vec], dtype=dtype)
+
+
 def mat2np(mat, dtype=np.float32):
-    out = np.zeros((len(mat.row), len(mat.col)), dtype=dtype)
-    for rid in range(len(mat.row)):
-        out[rid] = mat[rid]
-    return out
+    return np.array([vec2np(mat[rid]) for rid in range(len(mat.row))], dtype=dtype)
 
 
 def obj2np(obj, geo_type="position", dtype=np.float32, is_local=False):
     local_vertices = mesh2np(obj.data, geo_type, dtype, is_local)
     if is_local or geo_type == "normal":
         return local_vertices
-    world_matrix = mat2np(obj.matrix_world)
-    homo_vertices = np.ones((len(local_vertices), 4))
-    homo_vertices[:, 0:3] = local_vertices
-    for vid in range(len(local_vertices)):
-        homo_vertices[vid] = world_matrix @ homo_vertices[vid]
-    return homo_vertices[:, 0:3]
+    world_matrix = mat2np(obj.matrix_world, dtype=dtype)
+    homov = np.hstack((local_vertices, np.ones((len(local_vertices), 1))))
+    return np.array([world_matrix @ homov[vid]
+                     for vid in range(len(local_vertices))], dtype=dtype)[:, 0:3]
 
 
 def mesh2np(mesh, geo_type="position", dtype=np.float32, is_local=False):
     if geo_type not in ["position", "normal"]:
         raise Exception("The type should  be position or normal.")
-    vnum = len(mesh.vertices[0].co)
-    out = np.zeros((len(mesh.vertices), vnum))
-    for idx, vt in enumerate(mesh.vertices):
-        val = vt.co if geo_type == "position" else vt.normal
-        out[idx] = np.array([val[0], val[1], val[2]])
-    return out
+    return np.array([
+        vec2np(vt.co if geo_type == "position" else vt.normal)
+        for vt in mesh.vertices], dtype=dtype)
--- bnp/io.py
+++ bnp/io.py
@@ -3,7 +3,6 @@ import numpy as np
 from bnp.conversion import obj2np
 
 
-def load_geom(obj_name):
+def load_geom(obj_name, geo_type="position", dtype=np.float32, is_local=False):
     obj = bpy.context.scene.objects[obj_name]
-    positions = obj2np(obj, "position")
-    return positions
+    return obj2np(obj, geo_type=geo_type, dtype=dtype, is_local=is_local)
