39de6cdb25129b9c3011486acb862c34d97a0b1d
mocobt <mocobt@gmail.com>
2020-07-07 03:58:48 +0900

    add comments

--- bnp/conversion.py
+++ bnp/conversion.py
@@ -8,7 +8,7 @@ def any2np(obj, dtype=np.float32, is_local=False):
         return vec2np(obj, dtype=dtype)
     if type(obj) == mathutils.Matrix:
         return mat2np(obj, dtype=dtype)
-    elif type(obj) == bpy.types.ObjectType:
+    elif type(obj) == bpy.types.Object:
         return obj2np(obj, dtype=dtype, is_local=is_local)
     elif type(obj) == bpy.types.Mesh:
         return mesh2np(obj, dtype=dtype, is_local=is_local)
@@ -25,18 +25,20 @@ def mat2np(mat, dtype=np.float32):
 
 
 def obj2np(obj, geo_type="position", dtype=np.float32, is_local=False):
-    local_vertices = mesh2np(obj.data, geo_type, dtype, is_local)
+    # Input: obj(bpy.types.Object), Output: positions or normals
+    local_verts = mesh2np(obj.data, geo_type, dtype, is_local)  # (vtx_num, 3)
     if is_local or geo_type == "normal":
-        return local_vertices
-    world_matrix = mat2np(obj.matrix_world, dtype=dtype)
-    homov = np.hstack((local_vertices, np.ones((len(local_vertices), 1))))
-    return np.array([world_matrix @ homov[vid]
-                     for vid in range(len(local_vertices))], dtype=dtype)[:, 0:3]
+        return local_verts  # (vtx_num, 3)
+    world_matrix = mat2np(obj.matrix_world, dtype=dtype)  # (4, 4)
+    homov = np.hstack((local_verts, np.ones(
+        (len(local_verts), 1))))  # (vtx_num, 4)
+    return np.array([world_matrix @ v for v in homov], dtype=dtype)[:, 0:3]
 
 
 def mesh2np(mesh, geo_type="position", dtype=np.float32, is_local=False):
+    # Input: mesh(bpy.types.Mesh), Output: positions or normals
     if geo_type not in ["position", "normal"]:
         raise Exception("The type should  be position or normal.")
     return np.array([
-        vec2np(vt.co if geo_type == "position" else vt.normal)
-        for vt in mesh.vertices], dtype=dtype)
+        vec2np(v.co if geo_type == "position" else v.normal)
+        for v in mesh.vertices], dtype=dtype)  # (vtx_num, 3)
