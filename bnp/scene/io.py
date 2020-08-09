06ee7b1ce65ab42021bafada95af9973be3f25f4
mocobt <mocobt@gmail.com>
2020-08-10 06:23:41 +0900

    add render function

--- bnp/scene/io.py
+++ bnp/scene/io.py
@@ -23,6 +23,7 @@ def import_geom(filepath: str, keep_vertex_order=True, **kwargs):
         bpy.ops.import_mesh.stl(filepath=filepath, **kwargs)
     else:
         raise Exception("Illegal extension")
+    print(f"Imported {filepath}")
 
 
 def export_geom(filepath: str, obj: bpy.types.Object,
@@ -51,7 +52,28 @@ def export_geom(filepath: str, obj: bpy.types.Object,
 
 
 def render(filepath: str, camera: bpy.types.Object = bpy.context.scene.camera, animation: bool = False,
-           render: bpy.types.RenderSettings = bpy.context.scene.render, write_still=True):
+           frame_start: int = bpy.context.scene.frame_current, frame_end: int = bpy.context.scene.frame_current + 1,
+           fps: float = 30.0, render: bpy.types.RenderSettings = bpy.context.scene.render, write_still=True):
     bpy.context.scene.camera = camera
+    bpy.context.scene.frame_start = frame_start
+    bpy.context.scene.frame_end = frame_end
+    render.fps = fps
+
+    ext = filepath.split(".")[-1].lower()
+    if ext == "png":
+        render.image_settings.file_format = "PNG"
+    elif ext == "bmp":
+        render.image_settings.file_format = "BMP"
+    elif ext == "jpg" or ext == "jpeg":
+        render.image_settings.file_format = "JPEG"
+    elif ext == "exr":
+        render.image_settings.file_format = "OPEN_EXR"
+    elif ext == "hdr":
+        render.image_settings.file_format = "HDR"
+    elif ext == "mp4":
+        render.image_settings.file_format = "FFMPEG"
+        render.ffmpeg.format = "MPEG4"
+        render.ffmpeg.codec = "H264"
+
     render.filepath = filepath
     bpy.ops.render.render(animation=animation, write_still=write_still)
--- examples/004_set_camera_and_render_image.py
+++ examples/004_set_camera_and_render_image.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     KRt = (K @ Rt).reshape(1, 3, 4)
     projected_points = (KRt @ vertices).reshape(-1, 3)
     projected_points = projected_points / projected_points[:, 2].reshape(-1, 1)
-    projected_points /= 100  # forvisualization
+    projected_points /= 100  # for visualization
     bnp.scene.put_cubes(projected_points, size=0.10, sampling_rate=3)
     P = K @ Rt
 
@@ -31,3 +31,6 @@ if __name__ == '__main__':
     camera3 = bnp.create_camera("debug_reconstructed_camera2", K=K, Rt=Rt, scale=1.0, use_cv_coord=True)
 
     bnp.scene.render(str(LIBRARY_ROOT_PATH / "assets" / "render_out.png"), camera3)
+    bnp.scene.render(str(LIBRARY_ROOT_PATH / "assets" / "render_out.exr"), camera3)
+    bnp.scene.render(str(LIBRARY_ROOT_PATH / "assets" / "render_out.mp4"), camera3,
+                     animation=True, frame_start=0, frame_end=5)
